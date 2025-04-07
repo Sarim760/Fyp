70eb5842812b284b365fa407e56ab3b54d3d5c60
S123am-12 <39978@students.riphah.edu.pk>
2025-04-07 20:16:18 +0500

    Firebase and splash screen half

---
+++ lib/screens/On_board_Screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class OnBoardScreen extends StatefulWidget {
+  const OnBoardScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoardScreen> createState() => _OnBoardScreenState();
+}
+
+class _OnBoardScreenState extends State<OnBoardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("OnBoardScreen"),
+      ),
+    );
+  }
+}
