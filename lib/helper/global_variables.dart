f21829562fd7081877b835d81b4481b331a22067
Sarim760 <sarimamjad783@gmail.com>
2025-08-03 17:44:47 +0500

    Delete lib directory

--- lib/helper/global_variables.dart
+++
@@ -1,5 +0,0 @@
-class GlobalVariables {
-  final String apiString = "http://192.168.100.40:5000/api";
-  // final String apiString="https://fyp-backend-wheat.vercel.app/api";
-  final String localhost = "http://192.168.100.40:5000";
-}
